ec3a6e6af51e8dbcdb8de4bd1f67756c0723f9b8
thebabalola <t.babalolajoseph@gmail.com>
2026-01-30 04:08:11 +0100

    chore(repo): add gitignore entries and commit pending subgraph changes

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts";
 import {
   PlayerRegistered,
   GameCreated,
@@ -7,168 +7,185 @@ import {
   GameWon,
   GameForfeited,
   ChallengeCreated,
-  RewardClaimed
-} from "../generated/BlOcXTacToe/BlOcXTacToe"
-import { Player, Game, Move, RewardClaim, Protocol } from "../generated/schema"
+  RewardClaimed,
+} from "../generated/BlOcXTacToe/BlOcXTacToe";
+import { Player, Game, Move, RewardClaim, Protocol } from "../generated/schema";
 
 export function handleRewardClaimed(event: RewardClaimed): void {
   // Create a unique reward claim id
-  let id = event.params.gameId.toString() + "-" + event.block.number.toString() + "-" + event.logIndex.toString()
-  let rc = new RewardClaim(id)
-  rc.game = event.params.gameId.toString()
-  rc.winner = event.params.winner.toHex()
-  rc.amount = event.params.amount
-  rc.timestamp = event.block.timestamp
-  rc.save()
+  let id =
+    event.params.gameId.toString() +
+    "-" +
+    event.block.number.toString() +
+    "-" +
+    event.logIndex.toString();
+  let rc = new RewardClaim(id);
+  rc.game = event.params.gameId.toString();
+  rc.winner = event.params.winner.toHex();
+  rc.amount = event.params.amount;
+  rc.timestamp = event.block.timestamp;
+  rc.save();
 
   // Update protocol-wide metrics (singleton)
-  let protoId = "singleton"
-  let proto = Protocol.load(protoId)
+  let protoId = "singleton";
+  let proto = Protocol.load(protoId);
   if (!proto) {
-    proto = new Protocol(protoId)
-    proto.totalVolume = event.params.amount
-    proto.totalRewardsClaimed = BigInt.fromI32(1)
-    proto.totalFeesCollected = BigInt.fromI32(0)
+    proto = new Protocol(protoId);
+    proto.totalVolume = event.params.amount;
+    proto.totalRewardsClaimed = BigInt.fromI32(1);
+    proto.totalFeesCollected = BigInt.fromI32(0);
   } else {
-    proto.totalVolume = proto.totalVolume.plus(event.params.amount)
-    proto.totalRewardsClaimed = proto.totalRewardsClaimed.plus(BigInt.fromI32(1))
+    proto.totalVolume = proto.totalVolume.plus(event.params.amount);
+    proto.totalRewardsClaimed = proto.totalRewardsClaimed.plus(
+      BigInt.fromI32(1),
+    );
   }
-  proto.save()
+  proto.save();
 
   // Optionally update game finishedAt if not set
-  let game = Game.load(event.params.gameId.toString())
+  let game = Game.load(event.params.gameId.toString());
   if (game && !game.finishedAt) {
-    game.finishedAt = event.block.timestamp
-    game.status = "FINISHED"
-    game.save()
+    game.finishedAt = event.block.timestamp;
+    game.status = "FINISHED";
+    game.save();
   }
 }
 export function handlePlayerRegistered(event: PlayerRegistered): void {
-  let player = Player.load(event.params.player.toHex())
+  let player = Player.load(event.params.player.toHex());
   if (!player) {
-    player = new Player(event.params.player.toHex())
-    player.address = event.params.player
-    player.rating = BigInt.fromI32(1000)
-    player.totalGames = BigInt.fromI32(0)
-    player.wins = BigInt.fromI32(0)
-    player.losses = BigInt.fromI32(0)
-    player.draws = BigInt.fromI32(0)
+    player = new Player(event.params.player.toHex());
+    player.address = event.params.player;
+    player.rating = BigInt.fromI32(1000);
+    player.totalGames = BigInt.fromI32(0);
+    player.wins = BigInt.fromI32(0);
+    player.losses = BigInt.fromI32(0);
+    player.draws = BigInt.fromI32(0);
   }
-  player.username = event.params.username
-  player.save()
+  player.username = event.params.username;
+  player.save();
 }
 
 export function handleGameCreated(event: GameCreated): void {
-  let game = new Game(event.params.gameId.toString())
-  game.gameId = event.params.gameId
-  game.player1 = event.params.playerOne.toHex()
-  game.status = "CREATED"
-  game.betAmount = event.params.betAmount
+  let game = new Game(event.params.gameId.toString());
+  game.gameId = event.params.gameId;
+  game.player1 = event.params.playerOne.toHex();
+  game.status = "CREATED";
+  game.betAmount = event.params.betAmount;
   // boardSize is param 3 (index 3)
-  game.boardSize = event.parameters[3].value.toI32()
+  game.boardSize = event.parameters[3].value.toI32();
   // tokenAddress is param 4 (index 4)
-  game.token = event.parameters[4].value.toAddress()
-  game.createdAt = event.block.timestamp
-  game.players = [event.params.playerOne.toHex()]
-  game.save()
+  game.token = event.parameters[4].value.toAddress();
+  game.createdAt = event.block.timestamp;
+  game.players = [event.params.playerOne.toHex()];
+  game.save();
 }
 
 export function handleGameJoined(event: GameJoined): void {
-  let game = Game.load(event.params.gameId.toString())
+  let game = Game.load(event.params.gameId.toString());
   if (game) {
-    game.player2 = event.params.playerTwo.toHex()
-    game.status = "ACTIVE"
-    let players = game.players
-    players.push(event.params.playerTwo.toHex())
-    game.players = players
-    game.save()
+    game.player2 = event.params.playerTwo.toHex();
+    game.status = "ACTIVE";
+    let players = game.players;
+    players.push(event.params.playerTwo.toHex());
+    game.players = players;
+    game.save();
   }
 }
 
 export function handleMovePlayed(event: MovePlayed): void {
-  let moveId = event.params.gameId.toString() + "-" + event.block.number.toString() + "-" + event.logIndex.toString()
-  let move = new Move(moveId)
-  move.game = event.params.gameId.toString()
-  move.player = event.params.player.toHex()
-  move.row = event.params.moveIndex
-  move.col = 0 
-  move.symbol = 0
-  move.timestamp = event.block.timestamp
-  move.save()
+  let moveId =
+    event.params.gameId.toString() +
+    "-" +
+    event.block.number.toString() +
+    "-" +
+    event.logIndex.toString();
+  let move = new Move(moveId);
+  move.game = event.params.gameId.toString();
+  move.player = event.params.player.toHex();
+  move.row = event.params.moveIndex;
+  move.col = 0;
+  move.symbol = 0;
+  move.timestamp = event.block.timestamp;
+  move.save();
 }
 
 export function handleGameWon(event: GameWon): void {
-  let game = Game.load(event.params.gameId.toString())
+  let game = Game.load(event.params.gameId.toString());
   if (game) {
-    game.status = "FINISHED"
-    game.finishedAt = event.block.timestamp
-    let winnerAddress = event.params.winner.toHex()
-    game.winner = winnerAddress
-    
+    game.status = "FINISHED";
+    game.finishedAt = event.block.timestamp;
+    let winnerAddress = event.params.winner.toHex();
+    game.winner = winnerAddress;
+
     // Update stats
-    let winner = Player.load(winnerAddress)
+    let winner = Player.load(winnerAddress);
     if (winner) {
-      winner.wins = winner.wins.plus(BigInt.fromI32(1))
-      winner.totalGames = winner.totalGames.plus(BigInt.fromI32(1))
-      winner.save()
+      winner.wins = winner.wins.plus(BigInt.fromI32(1));
+      winner.totalGames = winner.totalGames.plus(BigInt.fromI32(1));
+      winner.save();
     }
-    
-    let player1Id = game.player1
-    let player2Id = game.player2
+
+    let player1Id = game.player1;
+    let player2Id = game.player2;
     if (player2Id) {
-       let loserId = winnerAddress == player1Id ? player2Id : player1Id
-       let loser = Player.load(loserId)
-       if (loser) {
-         loser.losses = loser.losses.plus(BigInt.fromI32(1))
-         loser.totalGames = loser.totalGames.plus(BigInt.fromI32(1))
-         loser.save()
-       }
+      let loserId = winnerAddress == player1Id ? player2Id : player1Id;
+      let loser = Player.load(loserId);
+      if (loser) {
+        loser.losses = loser.losses.plus(BigInt.fromI32(1));
+        loser.totalGames = loser.totalGames.plus(BigInt.fromI32(1));
+        loser.save();
+      }
     }
-    game.save()
+    game.save();
   }
 }
 
 export function handleGameForfeited(event: GameForfeited): void {
-  let game = Game.load(event.params.gameId.toString())
+  let game = Game.load(event.params.gameId.toString());
   if (game) {
-    game.status = "FINISHED"
-    game.finishedAt = event.block.timestamp
-    let winnerId = event.params.winner.toHex()
-    game.winner = winnerId
-    
-    let winner = Player.load(winnerId)
+    game.status = "FINISHED";
+    game.finishedAt = event.block.timestamp;
+    let winnerId = event.params.winner.toHex();
+    game.winner = winnerId;
+
+    let winner = Player.load(winnerId);
     if (winner) {
-      winner.wins = winner.wins.plus(BigInt.fromI32(1))
-      winner.totalGames = winner.totalGames.plus(BigInt.fromI32(1))
-      winner.save()
+      winner.wins = winner.wins.plus(BigInt.fromI32(1));
+      winner.totalGames = winner.totalGames.plus(BigInt.fromI32(1));
+      winner.save();
     }
-    
-    let player1Id = game.player1
-    let player2Id = game.player2
+
+    let player1Id = game.player1;
+    let player2Id = game.player2;
     if (player2Id) {
-       let forfeiterId = winnerId == player1Id ? player2Id : player1Id
-       let loser = Player.load(forfeiterId)
-       if (loser) {
-         loser.losses = loser.losses.plus(BigInt.fromI32(1))
-         loser.totalGames = loser.totalGames.plus(BigInt.fromI32(1))
-         loser.save()
-       }
+      let forfeiterId = winnerId == player1Id ? player2Id : player1Id;
+      let loser = Player.load(forfeiterId);
+      if (loser) {
+        loser.losses = loser.losses.plus(BigInt.fromI32(1));
+        loser.totalGames = loser.totalGames.plus(BigInt.fromI32(1));
+        loser.save();
+      }
     }
-    game.save()
+    game.save();
   }
 }
 
 export function handleChallengeCreated(event: ChallengeCreated): void {
-  let game = new Game(event.params.challengeId.toString())
-  game.gameId = event.params.challengeId
-  game.player1 = event.params.challenger.toHex()
-  game.player2 = event.params.challenged.toHex()
+  let game = new Game(event.params.challengeId.toString());
+  game.gameId = event.params.challengeId;
+  game.player1 = event.params.challenger.toHex();
+  game.player2 = event.params.challenged.toHex();
   // betAmount is param 3 (index 3)
-  game.betAmount = event.parameters[3].value.toBigInt()
-  game.boardSize = 3 
-  game.token = Bytes.fromHexString("0x0000000000000000000000000000000000000000")
-  game.createdAt = event.block.timestamp
-  game.status = "CREATED"
-  game.players = [event.params.challenger.toHex(), event.params.challenged.toHex()]
-  game.save()
+  game.betAmount = event.parameters[3].value.toBigInt();
+  game.boardSize = 3;
+  game.token = Bytes.fromHexString(
+    "0x0000000000000000000000000000000000000000",
+  );
+  game.createdAt = event.block.timestamp;
+  game.status = "CREATED";
+  game.players = [
+    event.params.challenger.toHex(),
+    event.params.challenged.toHex(),
+  ];
+  game.save();
 }
--- subgraph/tests/reward-claim-utils.ts
+++ subgraph/tests/reward-claim-utils.ts
@@ -1,14 +1,30 @@
-import { newMockEvent } from "matchstick-as/assembly/index"
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
-import { RewardClaimed } from "../generated/BlOcXTacToe/BlOcXTacToe"
+import { newMockEvent } from "matchstick-as/assembly/index";
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts";
+import { RewardClaimed } from "../generated/BlOcXTacToe/BlOcXTacToe";
 
-export function createRewardClaimedEvent(gameId: BigInt, winner: Address, amount: BigInt): RewardClaimed {
-  let event = changetype<RewardClaimed>(newMockEvent())
+export function createRewardClaimedEvent(
+  gameId: BigInt,
+  winner: Address,
+  amount: BigInt,
+): RewardClaimed {
+  let event = changetype<RewardClaimed>(newMockEvent());
 
-  event.parameters = new Array()
-  event.parameters.push(new ethereum.EventParam("gameId", ethereum.Value.fromUnsignedBigInt(gameId)))
-  event.parameters.push(new ethereum.EventParam("winner", ethereum.Value.fromAddress(winner)))
-  event.parameters.push(new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount)))
+  event.parameters = new Array();
+  event.parameters.push(
+    new ethereum.EventParam(
+      "gameId",
+      ethereum.Value.fromUnsignedBigInt(gameId),
+    ),
+  );
+  event.parameters.push(
+    new ethereum.EventParam("winner", ethereum.Value.fromAddress(winner)),
+  );
+  event.parameters.push(
+    new ethereum.EventParam(
+      "amount",
+      ethereum.Value.fromUnsignedBigInt(amount),
+    ),
+  );
 
-  return event
+  return event;
 }
